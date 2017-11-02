00cb6778ec41688492e78c112ba9569e8c11e997
getHecked <arjn.nair@gmail.com>
2017-11-02 13:51:08 +0100

    Update

--- backend/__init__.py
+++ backend/__init__.py
@@ -18,7 +18,7 @@ cards = {
 }
 
 def process_user_query(query_string):
-    return [f"Hi {name}!\n" for name in query_string.split(' ') if name[0].isupper()==True]
+    return 'Hi'
 
 
 def get_card():
